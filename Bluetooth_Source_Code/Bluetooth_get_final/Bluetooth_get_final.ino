8239b64158d500d5e7bc4a093e9b66e813236701
DA-Tripod <mrgamestest@gmail.com>
2013-11-13 10:45:33 +0100

    latest bluetooth versions

--- Bluetooth_Source_Code/Bluetooth_get_final/Bluetooth_get_final.ino
+++ Bluetooth_Source_Code/Bluetooth_get_final/Bluetooth_get_final.ino
@@ -1,7 +1,6 @@
 #include <SoftwareSerial.h>  
 #include <Servo.h>
 #include <stdlib.h>
-//#include <string.h>
 
 int bluetoothTx = 10;  // TX-O pin of bluetooth mate, Arduino D2
 int bluetoothRx = 11;  // RX-I pin of bluetooth mate, Arduino D3
@@ -10,8 +9,6 @@ SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 Servo myservo;
 int counter = 0;
 char x[4];
-//String test;
-//char sz[] = "hallo";
 
 // plug the pins TX-0 to Arduino D2 and the RX-I to Arduino D3
 // before uploading the sketch make sure that the bluetooth mate isn't supplied with voltage.
@@ -29,70 +26,34 @@ void setup()
   bluetooth.println("U,9600,N");  // Temporarily Change the baudrate to 9600, no parity
   bluetooth.println("---");
   bluetooth.begin(9600);          // Start bluetooth serial at 9600
-  /*bluetooth.print("$$$");
-  delay(100);
-  bluetooth.println("SM,0");*/ 
+  delay(1000);
 }
 
 void loop()
 {
-  Serial.println(bluetooth.read());
-  /*int n = 0;
-  
- // if(bluetooth.available()){
-    n = bluetooth.read();
-    if(n == 59){
-       Serial.println();
-    }else{  
-      n = n*4;
-      if(n < 1024 && n >= 0){
-        Serial.print(n);
-        myservo.write(map(n, 0, 1024, 0, 179));
-     } 
-  }
-  */
   if(bluetooth.available() && counter <= 9999)  // If the bluetooth sent any characters
   {
     
     x[counter] = bluetooth.read();
-    if(x[counter] != ';'){
+   
+   //a bunch of \0s is sent at the beginning
+   
+    if(x[counter] != ';' && x[counter] != '\0'){
       counter++;
+    }else if(x[counter] == '\0'){
     }else{
       int n;
       n = atoi(x);
-      Serial.println();
-      Serial.print(n);
+      Serial.println(n);
       
       if(n < 1024 && n >= 0){
         myservo.write(map(n, 0, 1024, 0, 179));
       }
+      
       for(int i = 0; i < 4; i++){
         x[i] = 0;
       }
       counter = 0;
-    }    
-  }
-  
-  if(Serial.available())  // If stuff was typed in the serial monitor
-  {
-    // Send any characters the Serial monitor prints to the bluetooth
-    bluetooth.print((char)Serial.read());
-  }
-  // and loop forever and ever!
-  
-  //delay(100);
-  
- /* if(bluetooth.available()){
-    Serial.println((char)bluetooth.read());
-  }*/
-  
-  if(bluetooth.available()){
-    temp = (char)bluetooth.read(); 
-    
-    if(temp != ';'){
-      Serial.print(temp)
-    }else{
-      Serial.println();
-    }
+    }   
   }
 }
