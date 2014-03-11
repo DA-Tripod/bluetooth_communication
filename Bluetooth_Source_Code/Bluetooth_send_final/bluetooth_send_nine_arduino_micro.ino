fbfbb58dc4448ce175d1b2e4876733cde844c99f
DA-Tripod <mrgamestest@gmail.com>
2014-03-11 18:10:16 +0100

    Create bluetooth_send_nine_arduino_micro.ino

---
+++ Bluetooth_Source_Code/Bluetooth_send_final/bluetooth_send_nine_arduino_micro.ino
@@ -0,0 +1,58 @@
+#include <SoftwareSerial.h>  
+
+int bluetoothTx = 12;  // TX-O pin of bluetooth mate, Arduino Micro D12
+int bluetoothRx = 11;  // RX-I pin of bluetooth mate, Arduino Micro D11
+
+SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
+
+void setup()
+{
+   bluetooth.begin(115200);        // The Bluetooth Mate defaults to 51300bps
+   delay(330);                     // IMPORTANT DELAY! (Minimum ~276ms)
+   bluetooth.print("$$$");         // Enter command mode
+   delay(100);                      // IMPORTANT DELAY! (Minimum ~10ms)
+   bluetooth.println("U,9600,N");  // Temporarily Change the baudrate to 9600, no parity
+   bluetooth.println("---");
+   bluetooth.begin(9600);          // Start bluetooth serial at 9600
+   delay(300);
+}
+
+void loop()
+{
+   bluetooth.print(111);
+   delay(5);
+   bluetooth.print('d');  //daumen
+   delay(5);
+   bluetooth.print(222);
+   delay(5);
+   bluetooth.print('h');  //hauptfinger/zeigefinger
+   delay(5);
+   bluetooth.print(333);
+   delay(5);
+   bluetooth.print('m');  //mittelfinger
+   delay(5);
+   bluetooth.print(444);
+   delay(5);
+   bluetooth.print('r');  //ringfinger
+   delay(5);
+   bluetooth.print(555);
+   delay(5);
+   bluetooth.print('k');  //kleiner finger
+   delay(5);
+   bluetooth.print(666);
+   delay(5);
+   bluetooth.print('x');  //x Koordinate (Ort)
+   delay(5);
+   bluetooth.print(777);
+   delay(5);
+   bluetooth.print('y');  //y Koordinate (Ort)
+   delay(5);
+   bluetooth.print(888);
+   delay(5);
+   bluetooth.print('z');  //z Koordinate (Ort)
+   delay(5);
+   bluetooth.print(999);
+   delay(5);
+   bluetooth.print('w');  //Winkel/Drehung der Hand
+   delay(5);  
+}
